37242e8116c7196e598b1c808f05a063a4f80e36
spriyanka2244 <66846210+spriyanka2244@users.noreply.github.com>
2021-06-05 13:15:30 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>History of javascript</h1>
+      <p>
+      JavaScript was invented by Brendan Eich in 1995.It was developed for Netscape 2, 
+      and became the ECMA-262 standard in 1997.
+      </p>
+      
+    </div>
+    
+  );
+}
+
+export default Note;
